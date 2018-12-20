c6d9dbd5c9857d1969fc7f1841e56759c5733e54
rubenRodrigues <rubenrodrigues666@gmail.com>
2018-12-20 01:01:08 +0000

    initial commit

---
+++ src/Components/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import './App.css';
+import Navbar from './Navbar'
+import 'bootstrap/dist/css/bootstrap.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+
+
+
+
+
+
+const BasicExample = () => (
+  <Router>
+    <div>
+      <Navbar/>
+      
+      
+
+    </div>
+  </Router>
+);
+
+
+
+
+
+export default BasicExample;
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem
+} from 'reactstrap';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Home from './Home';
+
+export default class Example extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+  render() {
+    return (
+      <Router>
+        <div>
+          <Navbar color="dark" dark expand="md">
+            <NavbarBrand href="/">Tribal Wars</NavbarBrand>
+            <NavbarToggler onClick={this.toggle} />
+            <Collapse isOpen={this.state.isOpen} navbar>
+              <Nav className="ml-auto" navbar>
+                <NavItem>
+                  <NavLink href="/leaderboards/">Leaderboards</NavLink>
+                </NavItem>
+
+                <UncontrolledDropdown nav inNavbar>
+                  <DropdownToggle nav caret>
+                    Options
+                </DropdownToggle>
+                  <DropdownMenu right>
+                    <DropdownItem>
+                      Option 1
+                  </DropdownItem>
+                    <DropdownItem divider />
+                    <DropdownItem>
+                      Logout
+                  </DropdownItem>
+                  </DropdownMenu>
+                </UncontrolledDropdown>
+              </Nav>
+            </Collapse>
+          </Navbar>
+
+          <Route exact path="/" component={Home} />
+
+
+        </div>
+      </Router>
+    );
+  }
+}
