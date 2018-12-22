44cd6689304f5159a3eb79244eb2b06cba2eb64f
rubenRodrigues <rubenrodrigues666@gmail.com>
2018-12-22 01:16:52 +0000

    added modal

--- src/Components/LoginView.jsx
+++ src/Components/LoginView.jsx
@@ -1,22 +1,25 @@
 import React, { Component } from 'react';
+import Modal from 'react-responsive-modal';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
-import  LoginForm from './LoginForm';
-import  RegisterForm  from './RegisterForm';
+import LoginForm from './LoginForm';
+import RegisterForm from './RegisterForm';
 import { ButtonToolbar, Button, toggleModal } from 'react-bootstrap';
 
 class LoginView extends Component {
   constructor(props, context) {
     super(props, context);
-    this.state = { isOpen: false };
+    this.state = { open: false};
     this.handleChange = this.handleChange.bind(this);
   }
 
-  toggleModal = () => {
-    this.setState({
-      isOpen: !this.state.isOpen
-    });
-  }
+  onOpenModal = () => {
+    this.setState({ open: true });
+  };
+
+  onCloseModal = () => {
+    this.setState({ open: false });
+  };
 
 
   handleChange(e) {
@@ -24,7 +27,8 @@ class LoginView extends Component {
   }
 
 
-  render() {    
+  render() {
+    const { open } = this.state;
     return (
       <div className="loginView">
         <div className="loginContainer">
@@ -34,15 +38,15 @@ class LoginView extends Component {
             </br>
             <div className="btnlv">
               <ButtonToolbar>
-                <Button onClick={this.toggleModal}
+                <Button onClick={() => this.setState({ open: true })}
                   bsSize="large"
                   className="btnLogin"
                   block>
-                  
+
                   <img src={require('./resources/images/login.png')}
                     className="loginImages" />
                   <h4 className="loginicons">Login</h4>
-                  
+
                 </Button>
                 <Button onClick={() => this.setState({ registerShow: true })}
                   bsSize="large"
@@ -51,6 +55,9 @@ class LoginView extends Component {
                   <img src={require('./resources/images/register.png')}
                     className="loginImages" /> <h4 className="loginicons">Registo</h4>
                 </Button>
+                <Modal open={open} onClose={this.onCloseModal} center>
+                  <LoginForm/>
+                </Modal>
               </ButtonToolbar>
             </div>
           </div>
