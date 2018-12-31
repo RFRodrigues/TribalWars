225fcc8cdf3d328dfaaf405f77d3b0fe5f774eef
ruben.rodrigues <ruben.rodrigues@accenture.com>
2018-12-31 16:43:25 +0000

    firebase connection and modal fix

--- src/Components/LoginForm.jsx
+++ src/Components/LoginForm.jsx
@@ -1,44 +1,101 @@
 import React, { Component } from 'react';
-import { Button, Modal } from 'react-bootstrap';
+import { FormControl, FormGroup, ControlLabel, HelpBlock, Checkbox, Radio, Button, Col, Form, Modal } from 'react-bootstrap';
 import './App.css';
 import PropTypes from 'prop-types';
+import firebase from "firebase";
+
+var config = {
+  apiKey: "AIzaSyBbGn-CM2XXJAmhkTERlsGDRyYryij9D4g",
+  authDomain: "tribalwars-15493.firebaseapp.com",
+  databaseURL: "https://tribalwars-15493.firebaseio.com",
+  projectId: "tribalwars-15493",
+  storageBucket: "tribalwars-15493.appspot.com",
+  messagingSenderId: "1009323283348"
+};
+firebase.initializeApp(config);
+
+
+
 
 class LoginForm extends React.Component {
-  render() {
-    if(!this.props.show) {
-      return null;
-    }
-    //CSS
-     // The gray background
-     const backdropStyle = {
-      position: 'fixed',
-      top: 0,
-      bottom: 0,
-      left: 0,
-      right: 0,
-      backgroundColor: 'rgba(0,0,0,0.3)',
-      padding: 50
+
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
+
     };
 
-    // The modal "window"
-    const modalStyle = {
-      backgroundColor: '#fff',
-      borderRadius: 5,
-      maxWidth: 500,
-      minHeight: 300,
-      margin: '0 auto',
-      padding: 30
-    };
-    //ENDCSS
+    this.handleChange = this.handleChange.bind(this);
+
+  }
+
+  handleChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+
+  componentWillMount() {
+
+  }
+
+
+
+
+  handleSubmit() {
+
+
+    var db = firebase.database().ref('/');
+    db.on('value', function (snapshot) {
+      var users = snapshot.val().Users;
+      console.log(users)
+
+    });
+
+
+  }
+
+  render() {
     return (
-      <div className="backdrop" style={{backdropStyle}}>
-        <div className="modal" style={{modalStyle}}>
+      <div className="backdrop">
+        <Form horizontal>
+          <FormGroup controlId="formHorizontalEmail">
+            <Col componentClass={ControlLabel} sm={2}>
+              Email
+          </Col>
+            <Col sm={10}>
+              <FormControl type="email" placeholder="Email" />
+            </Col>
+          </FormGroup>
+
+          <FormGroup controlId="formHorizontalPassword">
+            <Col componentClass={ControlLabel} sm={2}>
+              Password
+          </Col>
+            <Col sm={10}>
+              <FormControl type="password" placeholder="Password" />
+            </Col>
+          </FormGroup>
+
+          <FormGroup>
+            <Col smOffset={2} sm={10}>
+              <Checkbox>Remember me</Checkbox>
+            </Col>
+          </FormGroup>
+
+          <FormGroup>
+            <Col smOffset={2} sm={10}>
+              <Button onClick={() => this.handleSubmit()}>Sign in</Button>
+            </Col>
+          </FormGroup>
+        </Form>
+
+
+        <div className="modal">
           {this.props.children}
 
           <div className="footer">
-            <button onClick={this.props.onClose}>
-              Close
-            </button>
+
           </div>
         </div>
       </div>
