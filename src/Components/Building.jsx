8c4438915ef0bcf72d388b2f01083d64f0616c32
Ruben Rodrigues <ruben.rodrigues@vortal.biz>
2020-05-30 03:14:15 +0100

    yessss

--- src/Components/Building.jsx
+++ src/Components/Building.jsx
@@ -1,56 +1,55 @@
-import React, { Component } from 'react';
-import Modal from 'react-responsive-modal';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.css';
-import BuildingModal from './BuildingModal';
-import { ButtonToolbar, Button, toggleModal } from 'react-bootstrap';
-
+import React, { Component } from "react";
+import { Modal, Button } from "antd";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+import BuildingModal from "./BuildingModal";
 
 class Building extends Component {
-    constructor(props, context) {
-        super(props, context);
-        this.state = {
-            open: false,
-            nome: "",
-            nivel: 0,
-            imagem: ""
-        };
-        this.handleChange = this.handleChange.bind(this);
-    }
-
-    handleChange(e) {
-        this.setState({ [e.target.name]: e.target.value });
-    }
-
-    componentWillMount(){
-
-        this.setState({imagem: localStorage.getItem(this.props.nome)});
-        
-    }
-
-
-    onCloseModal = () => {
-        this.setState({ open: false });
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      open: false,
+      nome: "",
+      nivel: 0,
+      imagem: "",
     };
+    this.handleChange = this.handleChange.bind(this);
+  }
 
+  handleChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
 
-    render() {
-        const { open } = this.state;
+  componentWillMount() {
+    this.setState({ imagem: localStorage.getItem(this.props.nome) });
+  }
 
-        return (
-            <div className="building" >
-                <Button  onClick={() => this.setState({ open: true })}
-                    bsSize="large"
-                    className="btnBuilding"
-                    block>
-                    <img src={this.state.imagem}/> 
-                </Button>
-                <Modal open={open} onClose={this.onCloseModal} center>
-                    <BuildingModal />
-                </Modal>
-            </div>
-        );
-    }
+  onCloseModal = () => {
+    this.setState({ open: false });
+  };
+
+  render() {
+    const { open } = this.state;
+
+    return (
+      <div className="building">
+        <Button
+          onClick={() => this.setState({ open: true })}
+          className="btnBuilding"
+        >
+          <img src={this.state.imagem} />
+        </Button>
+
+        <Modal
+          title={"Edificio"}
+          visible={open}
+          onCancel={this.onCloseModal}
+        >
+          <BuildingModal />
+        </Modal>
+      </div>
+    );
+  }
 }
 
 export default Building;
