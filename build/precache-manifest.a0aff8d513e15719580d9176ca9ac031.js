fa3ebbb13fe5b18fc9f67318f921f08eb109788a
Ruben Rodrigues <ruben.rodrigues@vortal.biz>
2020-06-09 13:32:57 +0100

    build

---
+++ build/precache-manifest.a0aff8d513e15719580d9176ca9ac031.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "38a79c5e620ddeb956e2",
+    "url": "/static/css/main.0c82f71f.chunk.css"
+  },
+  {
+    "revision": "38a79c5e620ddeb956e2",
+    "url": "/static/js/main.2d2aa188.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a045ca78c715a631c6dc",
+    "url": "/static/css/2.56f01a67.chunk.css"
+  },
+  {
+    "revision": "a045ca78c715a631c6dc",
+    "url": "/static/js/2.c7e00ee0.chunk.js"
+  },
+  {
+    "revision": "214ca8e7ee4e4d9fd0d8fa49625f3f81",
+    "url": "/static/media/vinque.214ca8e7.ttf"
+  },
+  {
+    "revision": "e9c26d0d8c700c478aa746787ff13fb8",
+    "url": "/static/media/citylvl0.e9c26d0d.png"
+  },
+  {
+    "revision": "bc23871af3d83504cc30d7dc0bd98f85",
+    "url": "/static/media/splashScreen.bc23871a.jpg"
+  },
+  {
+    "revision": "96ac77a2aa32dcf2e588de0f5735d714",
+    "url": "/static/media/Main1.96ac77a2.png"
+  },
+  {
+    "revision": "447187b9116bcd62e60d29996a133cc0",
+    "url": "/static/media/login.447187b9.png"
+  },
+  {
+    "revision": "b16e2a7a5d22176f32922fd572c46e5a",
+    "url": "/static/media/register.b16e2a7a.png"
+  },
+  {
+    "revision": "5daedbe105a5d64f23f0c163e736008a",
+    "url": "/index.html"
+  }
+];
