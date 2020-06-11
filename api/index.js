3217710450b4d394bbded79cf09fec07411c4245
Rúben Filipe Santiago Lomba  Rodrigues <rrodrigues@smartconsulting.pt>
2020-06-11 18:48:55 +0100

    api deploy

--- api/index.js
+++ api/index.js
@@ -4,6 +4,10 @@ var bodyParser = require('body-parser');
 var app = express();
 const MongoClient = require('mongodb').MongoClient;
 
+const uri = "mongodb+srv://admin:admin@cluster0-4hukz.gcp.mongodb.net/?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri);
+
 // var firebaseConfig = {
 //   apiKey: "AIzaSyBbGn-CM2XXJAmhkTERlsGDRyYryij9D4g",
 //   authDomain: "tribalwars-15493.firebaseapp.com",
@@ -28,32 +32,44 @@ app.use(function (req, res, next) {
   next();
 });
 
-app.post("/api/usercreate/", (req, res) => {
+MongoClient.connect(uri, function (err, db) {
+
+  var dbo = db.db("tribalwars");
+
+  app.post("/api/usercreate/", (req, res) => {
 
 
-  // firebase
-  //   .database()
-  //   .ref("/users")
-  //   .push({ 
-  //     userinfo: { username: req.body.nickname, email: req.body.email, password: req.body.password },
-  //     resources: { wood: 0, iron: 0, clay: 0 },
-  //     buildings: { mainBuilding: 0, barrack: 0, warehouse: 0 }
-  //   });
+    // firebase
+    //   .database()
+    //   .ref("/users")
+    //   .push({ 
+    //     userinfo: { username: req.body.nickname, email: req.body.email, password: req.body.password },
+    //     resources: { wood: 0, iron: 0, clay: 0 },
+    //     buildings: { mainBuilding: 0, barrack: 0, warehouse: 0 }
+    //   });
 
-  //res.sendStatus(200);
-});
+    //res.sendStatus(200);
+  });
 
-//get all users info
-app.get("/api/usersinfo/", (req, res) => {
-  // var db = firebase.database().ref("/users");
-  // db.once("value", (snapshot) => {
-  //   var users = snapshot.val();
-  //   res.send(users);
-  // });
-  res.json([
-    { name: "William", location: "Abu Dhabi" },
-    { name: "Chris", location: "Vegas" }
-  ]);
+  //get all users info
+  app.get("/api/usersinfo/", async (req, res) => {
+
+    dbo.collection("Users").find({}).toArray((err, result) => {
+      if (err) {
+        res.send("Error in get users");
+      }
+      else {
+        res.send(result);
+      }
+    });
+    // var db = firebase.database().ref("/users");
+    // db.once("value", (snapshot) => {
+    //   var users = snapshot.val();
+    //   res.send(users);
+    // });
+
+  });
+
 });
 
 app.listen(5000);
