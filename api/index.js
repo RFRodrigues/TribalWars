90994a52f090b07faf73a8ad2b11fdc59457b590
Rúben Filipe Santiago Lomba  Rodrigues <rrodrigues@smartconsulting.pt>
2020-06-11 15:11:56 +0100

    yesss

--- api/index.js
+++ api/index.js
@@ -1,19 +1,20 @@
-var firebase = require("firebase");
+// var firebase = require("firebase");
 var express = require("express");
 var bodyParser = require('body-parser');
 var app = express();
+const MongoClient = require('mongodb').MongoClient;
 
-var firebaseConfig = {
-  apiKey: "AIzaSyBbGn-CM2XXJAmhkTERlsGDRyYryij9D4g",
-  authDomain: "tribalwars-15493.firebaseapp.com",
-  databaseURL: "https://tribalwars-15493.firebaseio.com",
-  projectId: "tribalwars-15493",
-  storageBucket: "tribalwars-15493.appspot.com",
-  messagingSenderId: "1009323283348",
-  appId: "1:1009323283348:web:465fb1bed784908d33f581",
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+// var firebaseConfig = {
+//   apiKey: "AIzaSyBbGn-CM2XXJAmhkTERlsGDRyYryij9D4g",
+//   authDomain: "tribalwars-15493.firebaseapp.com",
+//   databaseURL: "https://tribalwars-15493.firebaseio.com",
+//   projectId: "tribalwars-15493",
+//   storageBucket: "tribalwars-15493.appspot.com",
+//   messagingSenderId: "1009323283348",
+//   appId: "1:1009323283348:web:465fb1bed784908d33f581",
+// };
+// // Initialize Firebase
+// firebase.initializeApp(firebaseConfig);
 
 app.use(express.json());
 
@@ -29,28 +30,31 @@ app.use(function (req, res, next) {
 
 app.post("/api/usercreate/", (req, res) => {
 
-  firebase
-    .database()
-    .ref("/users")
-    .push({ 
-      userinfo: { username: req.body.nickname, email: req.body.email, password: req.body.password },
-      resources: { wood: 0, iron: 0, clay: 0 },
-      buildings: { mainBuilding: 0, barrack: 0, warehouse: 0 }
-    });
 
-  res.sendStatus(200);
+  // firebase
+  //   .database()
+  //   .ref("/users")
+  //   .push({ 
+  //     userinfo: { username: req.body.nickname, email: req.body.email, password: req.body.password },
+  //     resources: { wood: 0, iron: 0, clay: 0 },
+  //     buildings: { mainBuilding: 0, barrack: 0, warehouse: 0 }
+  //   });
+
+  //res.sendStatus(200);
 });
 
 //get all users info
 app.get("/api/usersinfo/", (req, res) => {
-  var db = firebase.database().ref("/users");
-  db.once("value", (snapshot) => {
-    var users = snapshot.val();
-    res.send(users);
-  });
+  // var db = firebase.database().ref("/users");
+  // db.once("value", (snapshot) => {
+  //   var users = snapshot.val();
+  //   res.send(users);
+  // });
+  res.json([
+    { name: "William", location: "Abu Dhabi" },
+    { name: "Chris", location: "Vegas" }
+  ]);
 });
 
 app.listen(5000);
 console.log("API is running");
-
-module.exports = app;
